1fe2269117e2c35b999712d1cd87215efa79bc3c
David G. Young <davidgyoung@gmail.com>
2015-04-19 20:46:44 -0400

    switch to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150419162556) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tags", force: true do |t|
     t.string   "tag_id"
     t.integer  "rssi"
